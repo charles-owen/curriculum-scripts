e3e4187cb510010f7299e7aeccb907613d75fa17
Dr. Charles B. Owen <charles@phibes.us>
2024-11-05 11:03:43 -0500

    Support for Windows classes-by-term

--- bin/classes-by-term.bat
+++ bin/classes-by-term.bat
@@ -1,2 +1,2 @@
-set currscripts=C:\Users\cbowen\curriculum-scripts
-%currscripts%\.venv\Scripts\python %currscripts%\classes-by-term-cleanup-gui.py
+set currscripts=C:\Users\charlesowen\curriculum-scripts
+%currscripts%\venv2\Scripts\python %currscripts%\classes-by-term-cleanup-gui.py
---
+++ bin/create-venv.bat
@@ -0,0 +1,3 @@
+python -m venv ..\venv2
+..\venv2\Scripts\pip install setuptools
+..\venv2\Scripts\python ..\setup.py install
